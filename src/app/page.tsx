2fc6f0c575b999f4f7a582af2ab1b731c5d8268f
Darya Pakhutova <daria.pakhutova@polyus-nt.ru>
2026-06-21 18:51:39 +0800

    create form component, form-field and validate

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,11 @@
+'use client';
+import { BookingForm } from '@/components/BookingForm/BookingForm';
+
 export default function Home() {
-  return <h1>Start</h1>;
+  return (
+    <main className='mx-auto max-w-md p-4'>
+      <h1 className='mb-4 text-2xl font-semibold'>Бронирование столика</h1>
+      <BookingForm status='idle' onSubmit={(data) => console.log(data)} />
+    </main>
+  );
 }
